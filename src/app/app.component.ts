beae028fbd8d61c3dd00ec9f2e370581a7686a02
Gino Luiggi Chirinos Plasencia <luiggiplasencia0823@gmail.com>
2020-03-21 02:31:43 +0100

    Aplicando google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
-import { Router, Event, NavigationEnd, NavigationStart, ActivationEnd } from '@angular/router';
+import { Router, Event, NavigationEnd, ActivationEnd } from '@angular/router';
 import { map, filter } from 'rxjs/operators';
+
+declare var gtag: any;
 
 @Component({
   selector: 'app-root',
@@ -15,19 +18,24 @@ export class AppComponent {
     private router: Router,
     private appTitle: Title,
     private appMeta: Meta,
+    @Inject(PLATFORM_ID) private platformId: Object
   ) {
     this.getDataRoute().subscribe(data => {
-      const appTitle: string = `ANGULAR SSR - ${data.title}`;
+      const title: string = `ANGULAR SSR - ${data.title}`;
 
-      this.appTitle.setTitle(appTitle);
+      this.appTitle.setTitle(title);
 
       const metaTag: MetaDefinition = {
         name: 'description',
-        content: appTitle
+        content: title
       };
 
       this.appMeta.updateTag(metaTag);
     });
+
+    if (isPlatformBrowser(this.platformId)) {
+      this.googleAnalyticsListener();
+    }
   }
 
   getDataRoute() {
@@ -37,5 +45,18 @@ export class AppComponent {
         filter((event: ActivationEnd) => event.snapshot.firstChild === null),
         map(response => response.snapshot.data )
       );
+  }
+
+  googleAnalyticsListener(): void {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      )
+      .subscribe((event: NavigationEnd) => {
+        console.log('call googleAnalyticsListener');
+        gtag('config', 'UA-161414129-1', {
+          page_path: event.urlAfterRedirects
+        });
+      });
   }
 }
