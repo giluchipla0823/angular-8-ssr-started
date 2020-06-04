2a87a204c765615d473aad848fd46d16515aa2ee
Gino Luiggi Chirinos Plasencia <luiggiplasencia0823@gmail.com>
2020-06-04 10:34:59 +0200

    Implementando gulp purifyCSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
-import { SecurityModule } from './pages/security/security.module';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
+
+// import { BarecodeScannerLivestreamModule, BarecodeScannerLivestreamOverlayModule } from 'ngx-barcode-scanner';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,12 @@ import { FooterComponent } from './components/shared/footer/footer.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    /* BarecodeScannerLivestreamModule,
+    BarecodeScannerLivestreamOverlayModule */
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
